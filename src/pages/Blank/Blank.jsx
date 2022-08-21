ebb52c5d382a42faaf50d3bf2d40a6777142d1e9
Antrems <voropaefftema@yandex.ru>
2022-08-21 13:20:47 +0300

    Create Modal Component

--- src/pages/Blank/Blank.jsx
+++ src/pages/Blank/Blank.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BlankArea from '../../components/BlankArea/BlankArea'
 import { Link, useParams } from 'react-router-dom'
 import Button from '../../components/UI/Button/Button'
 import { useSelector } from 'react-redux'
 import { collection, getDocs, doc, deleteDoc, addDoc } from 'firebase/firestore'
 import { db } from '../../firebase'
+import Modal from '../../components/UI/Modal/Modal'
 
 const Blank = () => {
     const { id } = useParams()
     const blankList = useSelector(state => state.blanks.blanks)
     const currentBlank = blankList.find(blank => blank.id === id)
+    const [modalOpen, toggleModalStatus] = useState(false)
 
     function dateFormating(date) {
         return new Date(date).toLocaleDateString()
@@ -22,7 +24,7 @@ const Blank = () => {
         })
         try {
             await addDoc(collection(db, 'printedDoc'), currentBlank)
-            window.location.href = 'https://docs.google.com/document/d/1leSY1FMJm0NLmmIgUFiK-8zjrLgHAttkZGAeT87zX18/edit'
+            toggleModalStatus(!modalOpen)
         } catch (err) {
             console.log(err)
         }
@@ -86,6 +88,24 @@ const Blank = () => {
                     <Button onClick={() => printBlank()}>Печать</Button>
                 </div>
             </div>
+            {modalOpen && (
+                <Modal>
+                    <div className="modalTitle">Выберите шаблон договора</div>
+                    <ul className="ModalList">
+                        <li className="ModalItem">
+                            <a
+                                className="ModalLink"
+                                href="https://docs.google.com/document/d/1leSY1FMJm0NLmmIgUFiK-8zjrLgHAttkZGAeT87zX18"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                Стандартный, ЭПТС
+                            </a>
+                        </li>
+                    </ul>
+                    <Button onClick={() => toggleModalStatus(!modalOpen)}>Отмена</Button>
+                </Modal>
+            )}
         </div>
     )
 }
